e8522675369968582626ed038c5ce446834223c4
Daniil Pesotskyi <pesotckiydanya@gmail.com>
2023-03-19 23:25:37 +0200

    timer starting

---
+++ src/js/02-timer.js
@@ -0,0 +1,20 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+
+const refs = {
+    dateTimeInput: document.getElementById('datetime-picker'),
+    startButton: document.querySelector('[data-start]')
+}
+
+const options = {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+      console.log(selectedDates[0]);
+    },
+};
+
+
+flatpickr(refs.dateTimeInput, options)
