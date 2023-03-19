05eef51c18518f4785355f7dc6dadae52e698da8
Daniil Pesotskyi <pesotckiydanya@gmail.com>
2023-03-19 21:59:10 +0200

    color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,35 @@
+const refs = {
+    startButton: document.querySelector('[data-start]'),
+    stopButton: document.querySelector('[data-stop]'),
+    pageBody: document.querySelector('body'),
+}
 
+refs.startButton.addEventListener('click', startColorSwitcher)
+refs.stopButton.addEventListener('click', stopColorSwitcher)
+
+let intervalId = null;
+let isColorSwitcherActive = true;
+
+function startColorSwitcher() {
+    if(isColorSwitcherActive) {
+        isColorSwitcherActive = false;
+        intervalId = setInterval(changeBodyColor, 1000)
+        refs.stopButton.removeAttribute("disabled", "disabled")
+        refs.startButton.setAttribute("disabled", "disabled")
+    }
+}
+
+function stopColorSwitcher() {
+    isColorSwitcherActive = true;
+    clearInterval(intervalId)
+    refs.stopButton.setAttribute("disabled", "disabled")
+    refs.startButton.removeAttribute("disabled", "disabled")
+}
+
+function changeBodyColor() {
+    refs.pageBody.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
